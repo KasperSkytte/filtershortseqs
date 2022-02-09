b726463e7880d6c63a566c4ae89670f18754e331
Kasper Skytte Andersen <knaldhat@gmail.com>
2022-02-09 11:24:44 +0100

    show time elapsed in H:M:S format

--- main.cpp
+++ main.cpp
@@ -128,8 +128,16 @@ int main(int argc, char* argv[])
     cout << " done" << endl;
 
     auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
-    cout << "Process took: " << duration.count() << " seconds to complete" <<endl;
+    auto duration_h = chrono::duration_cast<chrono::hours>(stop - start).count();
+    auto duration_m = chrono::duration_cast<chrono::minutes>(stop - start).count();
+    auto duration_s = chrono::duration_cast<chrono::seconds>(stop - start).count();
+    cout << "Process took ";
+    cout << duration_h;
+    cout << "h:";
+    cout << duration_m;
+    cout << "m:";
+    cout << duration_s;
+    cout << "s to complete" << endl;
 
     return 0;
 }
